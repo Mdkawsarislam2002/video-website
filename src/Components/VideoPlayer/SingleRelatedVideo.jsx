eeea2c1f32a9850c310cba477c15af840d06ef8b
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-04-15 14:07:53 +0600

    fixed  Link issue

--- src/Components/VideoPlayer/SingleRelatedVideo.jsx
+++ src/Components/VideoPlayer/SingleRelatedVideo.jsx
@@ -22,29 +22,24 @@ const SingleRelatedVideo = ({ video }) => {
     <>
       <div className="w-full flex flex-row gap-2 mb-4">
         <div className="relative w-[168px] h-[94px] flex-none duration-300 hover:scale-[1.03]">
-          <Link to={`/video/${id}`}>
-            <img
-              src={thumbnail}
-              className="object-cover"
-              alt="Some video title"
-            />
-          </Link>
+          <img
+            src={thumbnail}
+            className="object-cover"
+            alt="Some video title"
+          />
+
           <p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
             {duration}
           </p>
         </div>
 
-        <Link to={"/video/" + id}>
+        <Link to={`/video/${id}`}>
           <div className="flex flex-col w-full">
-            <a href="#">
-              <p className="text-slate-900 text-sm font-semibold">{title}</p>
-            </a>
-            <a
-              className="text-gray-400 text-xs mt-2 hover:text-gray-600"
-              href="#"
-            >
+            <p className="text-slate-900 text-sm font-semibold">{title}</p>
+
+            <p className="text-gray-400 text-xs mt-2 hover:text-gray-600">
               Learn with Sumit
-            </a>
+            </p>
             <p className="text-gray-400 text-xs mt-1">
               {views} {date}
             </p>
